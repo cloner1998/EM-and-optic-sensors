91720ef82c4553c88eaa83e06ebb4280687dc4b6
cloner1998 <sajjadyami77@gmail.com>
2025-11-17 16:56:04 +0330

    [impl] implement active and passive formula for the EM sensors

--- model/emModel.py
+++ model/emModel.py
@@ -1,3 +1,4 @@
+
 from dataclass.emDataClass import EmSensorsParameters
 import numpy as np
 
@@ -9,7 +10,7 @@ class EMSensorsModel:
     def free_space_path_loss(self, R: float):
         return np.power(4 * np.pi * R / self.params.wavelength, 2)
 
-    def received_power(self, R: float):
+    def received_power_passive(self, R: float):
         P_t = self.params.P_t
         G_t = self.params.G_t
         G_r = self.params.G_r
@@ -17,30 +18,48 @@ class EMSensorsModel:
         L = self.params.L_system
         alpha = self.params.alpha
 
-        P_r = (np.power(P_t * G_t * G_r * (wavelength / (4 * np.pi * R)), 2) *
+        P_r = (P_t * G_t * G_r * np.power((wavelength / (4 * np.pi * R)), 2) *
                np.exp(-alpha * R) / L)
 
         return P_r
 
-    # ToDo(implement effective range for passive EM sensors)
+
+    def received_power_active(self, R: float):
+        P_t = self.params.P_t
+        G = self.params.G_t
+        wavelength = self.params.wavelength
+        cross_section = self.params.cross_section
+
+        P_r = (P_t * (G**2) * cross_section * (wavelength ** 2))/((np.pi ** 3) * R ** 4)
+        return P_r
+
 
     def effective_range(self) -> float:
         R_min, R_max = 1.0, 1e6
         tolerance = 1.0
+        if self.params.sensor_type.lower() == "active":
+            while R_max - R_min > tolerance:
+                R_mid = (R_max + R_min) / 2
+                P_r = self.received_power_active(R_mid)
 
-        while R_max - R_min > tolerance:
-            R_mid = (R_max + R_min) / 2
-            P_r = self.received_power(R_mid)
+                if P_r > self.params.P_r_min:
+                    R_min = R_mid
+                else:
+                    R_max = R_mid
+        elif self.params.sensor_type.lower() == "passive":
+            while R_max - R_min > tolerance:
+                R_mid = (R_max + R_min) / 2
+                P_r = self.received_power_passive(R_mid)
 
-            if P_r > self.params.P_r_min:
-                R_min = R_mid
-            else:
-                R_max = R_mid
-
+                if P_r > self.params.P_r_min:
+                    R_min = R_mid
+                else:
+                    R_max = R_mid
+        else:
+            raise NotImplementedError
         return R_min
 
     def angle_error(self) -> float:
-        """Calculate RMS angular error (radians) using Equation 10"""
         lambda_val = self.params.wavelength
         D = self.params.D
         SNR = self.params.snr_linear()
