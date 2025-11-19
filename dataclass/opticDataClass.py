3c9aa6918332ad1415c93aac3aebbd3295990e9d
cloner1998 <sajjadyami77@gmail.com>
2025-11-19 10:33:53 +0330

    [improve] improve opticDataClass and integrated with units. Add assertion

--- dataclass/opticDataClass.py
+++ dataclass/opticDataClass.py
@@ -1,26 +1,26 @@
 from dataclasses import dataclass
-
+from model.units import assert_positive, db2lin
 
 @dataclass
 class OpticSensorsParameters:
-    I_0: float
-    I_min: float
-    wavelength: float
-    beta: float
-
-    # imaging sensor parameter
-    D: float = 0.1
+    I_0: float              # W/m^2, source irradiance
+    I_min: float            # W/m^2, minimum detectable irradiance
+    wavelength: float       # m
+    beta: float             # extinction coefficient (1/m)
+    D: float = 0.1          # aperture diameter (m)
     pixel_pitch: float = 5e-6
     focal_length: float = 0.05
     SNR_db: float = 30.0
-
-    # non-imaging sensors
-    # ToDo(we should investigate more on this parameter for non-imaging sensors)
     spot_diameter: float = 1e-3
     sigma_pos_x: float = 1e-4
+    sensor_type: str = "imaging"
 
-    sensor_type: str = "imaging"  # it could be non-imaging
-
-    @property
-    def snr_linear(self) -> float:
-        return 10 ** (self.SNR_db / 10)
+    def __post_init__(self):
+        assert_positive("I_0", self.I_0)
+        assert_positive("I_min", self.I_min)
+        assert_positive("wavelength", self.wavelength)
+        if self.beta < 0:
+            raise ValueError("beta (extinction coefficient) should be >= 0 (1/m)")
+        assert_positive("D (aperture)", self.D)
+        assert_positive("pixel_pitch", self.pixel_pitch)
+        assert_positive("focal_length", self.focal_length)
